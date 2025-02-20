1b7500c58cc09c2140072944bd571302c2d06c68
admin <lqhung288@gmail.com>
2025-02-20 23:38:16 +0700

    backend setup

---
+++ src/main/java/com/example/pregnancy_tracking/PregnancyTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.example.pregnancy_tracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PregnancyTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PregnancyTrackingApplication.class, args);
+	}
+
+}
