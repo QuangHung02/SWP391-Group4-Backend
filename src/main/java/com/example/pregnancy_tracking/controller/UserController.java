8f5fb466cd973a1e9e26dbcc791c6a517c2aae8b
admin <lqhung288@gmail.com>
2025-02-22 21:01:41 +0700

    Update,get,show all

--- src/main/java/com/example/pregnancy_tracking/controller/UserController.java
+++ src/main/java/com/example/pregnancy_tracking/controller/UserController.java
@@ -1,4 +1,32 @@
 package com.example.pregnancy_tracking.controller;
 
+import com.example.pregnancy_tracking.dto.UserDTO;
+import com.example.pregnancy_tracking.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<UserDTO> getAllUsers() {
+        return userService.getAllUsers();
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> getUserById(@PathVariable Long id) {
+        UserDTO user = userService.getUserById(id);
+        return ResponseEntity.ok(user);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDTO> updateUser(@PathVariable Long id, @RequestBody UserDTO userDTO) {
+        return ResponseEntity.ok(userService.updateUser(id, userDTO));
+    }
 }
--- src/main/java/com/example/pregnancy_tracking/service/UserService.java
+++ src/main/java/com/example/pregnancy_tracking/service/UserService.java
@@ -1,4 +1,39 @@
 package com.example.pregnancy_tracking.service;
 
+import com.example.pregnancy_tracking.dto.UserDTO;
+import com.example.pregnancy_tracking.entity.User;
+import com.example.pregnancy_tracking.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<UserDTO> getAllUsers() {
+        List<User> users = userRepository.findAll();
+        return users.stream()
+                .map(user -> new UserDTO(user.getUsername(), user.getEmail(), user.getRole().name()))
+                .collect(Collectors.toList());
+    }
+
+    public UserDTO getUserById(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        return new UserDTO(user.getUsername(), user.getEmail(), user.getRole().name());
+    }
+    public UserDTO updateUser(Long id, UserDTO userDTO) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        user.setUsername(userDTO.getUsername());
+        user.setEmail(userDTO.getEmail());
+        userRepository.save(user);
+        return new UserDTO(user.getUsername(), user.getEmail(), user.getRole().name());
+    }
 }
